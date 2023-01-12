9aaf4ad51bc71180e3e9afefa2d03f04a46dd5ff
Buyaki <rittahbuyaki@gmail.com>
2023-01-12 14:44:02 +0300

    Add options for winning

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,16 @@ if(playGame){
     let playerOne = playerChoice.trim().toLowerCase();
     if(playerOne === "rock" || playerOne === "paper" || playerOne === "scissors"){
       let computerChoice = Math.floor(Math.random() * 3 + 1);
+      let computer = computerChoice === 1 ? "rock"
+        :computerChoice === 2 ? "paper"
+        :"scissors";
+
+        let result = playerOne === computer ? "Tie game"
+          :playerOne === "rock" && computer === "paper" ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+          :playerOne === "paper" && computer === "scisssors" ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+          :playerOne === "scisssors" && computer === "rock" ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+          :`playerOne: ${playerOne}\nComputer: ${computer}\nPlayerOne wins!`
+          alert(result);
     }else{
       alert("You didn't enter rock, paper or scissors")
     }

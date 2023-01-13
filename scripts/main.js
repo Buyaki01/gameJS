6491cadf4c9ac8ca338d0e6d311059d3a7086dbc
Buyaki <rittahbuyaki@gmail.com>
2023-01-13 13:47:13 +0300

    Refactor the game with Arrays

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,31 @@
 let playGame = confirm("Shall we play rock, paper or scissors");
 if(playGame){
   //Play Game
-  let playerChoice = prompt("Please enter rock, paper or scissors.")
-  if(playerChoice){
-    let playerOne = playerChoice.trim().toLowerCase();
-    if(playerOne === "rock" || playerOne === "paper" || playerOne === "scissors"){
-      let computerChoice = Math.floor(Math.random() * 3 + 1);
-      let computer = computerChoice === 1 ? "rock"
-        :computerChoice === 2 ? "paper"
-        :"scissors";
-
-        let result = playerOne === computer ? "Tie game"
+  while(playGame){
+    const playerChoice = prompt("Please enter rock, paper or scissors.");
+    if(playerChoice || playerChoice === ""){
+      const playerOne = playerChoice.trim().toLowerCase();
+      if(playerOne === "rock" || playerOne === "paper" || playerOne === "scissors"){
+        const computerChoice = Math.floor(Math.random() * 3 + 1);
+        const rpsArray = ["rock", "paper", "scissors"];
+        const computer = rpsArray[computerChoice];
+        const result = playerOne === computer ? "Tie game"
           :playerOne === "rock" && computer === "paper" ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
           :playerOne === "paper" && computer === "scisssors" ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
           :playerOne === "scisssors" && computer === "rock" ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
           :`playerOne: ${playerOne}\nComputer: ${computer}\nPlayerOne wins!`
           alert(result);
-        let playAgain = confirm("Play Again?");
-        playAgain ? location.reload() :alert("Ok, maybe next time")
+        playGame = confirm("Play Again?");
+        if(!playGame) alert("Ok, Thanks for playing");
+        continue;
+      }
+      else{
+        alert("You didn't enter rock, paper or scissors")
+      }
     }else{
-      alert("You didn't enter rock, paper or scissors")
+      alert("I guess you changed your mind. Maybe next time.");
+      break;
     }
-  }else{
-    alert("I guess you changed your mind. Maybe next time.")
   }
 }else{
   alert("Ok, maybe next time")
